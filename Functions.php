813d141b8638d07a4993af47a8bc7e87307ba21d
AghaieM2570 <maryamaghaie2570@gmail.com>
2022-06-01 13:36:33 +0430

    gd: functions

--- Functions.php
+++ Functions.php
@@ -2,41 +2,48 @@
 
 function loadData($section, $data)
 {
-    return app('matin-cacher')->loadData($section, $data);
+    $response = app('matin-cacher')->loadData($section, $data);
+    return $response === false ? $response : json_decode($response, true);
 }
 
 function cacheitem($item, $value = '')
 {
-    return app('matin-cacher')->setItem($item, $value);
+    $response = app('matin-cacher')->setItem($item, $value);
+    return $response === false ? $response : json_decode($response, true);
 }
 
 function removeCache($item)
 {
-    return app('matin-cacher')->forget($item);
+    $response = app('matin-cacher')->forget($item);
+    return $response === false ? $response : json_decode($response, true);
 }
 
-function getItem($section, $item = null)
+function getCachedItem($item)
 {
-    $item = $section . (empty($item) ? '' : ".$item");
-    return app('matin-cacher')->getItem($item);
+    $response = app('matin-cacher')->getItem($item);
+    return $response === false ? $response : json_decode($response, true)['value'] ?? null;
 }
 
 function databaseConfigs()
 {
-    return app('matin-cacher')->databaseConfigs(config('database.connections.mysql'));
+    $response = app('matin-cacher')->databaseConfigs(config('database.connections.mysql'));
+    return $response === false ? $response : json_decode($response, true);
 }
 
 function feedTable($tableName)
 {
-    return app('matin-cacher')->feedTable($tableName);
+    $response = app('matin-cacher')->feedTable($tableName);
+    return $response === false ? $response : json_decode($response, true);
 }
 
 function refreshTable($tableName)
 {
-    return app('matin-cacher')->refreshTable($tableName);
+    $response = app('matin-cacher')->refreshTable($tableName);
+    return $response === false ? $response : json_decode($response, true);
 }
 
 function getTableItem($tableName, $column, $value)
 {
-    return app('matin-cacher')->getTableItem($tableName, $column, $value);
+    $response = app('matin-cacher')->getTableItem($tableName, $column, $value);
+    return $response === false ? $response : json_decode($response, true);
 }
